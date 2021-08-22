250c86d916a3d1cf58af5b6d1e0c640bd2d406d9
Yisak <it.yisak.wondim@gmail.com>
2021-08-22 12:59:58 +0200

    creating user model

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,29 @@
+
+class UserModel {
+  final String ?id;
+  final String ?name;
+  final String ?email;
+  final String ?password;
+  final String ?re_password;
+  final bool ?administrator;
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+    this.password,
+    this.re_password,
+    this.administrator,
+  });
+
+  factory UserModel.fromJSON(Map<String,dynamic> jsonMap){
+    final result = UserModel(
+      id:jsonMap['id'],
+      name: jsonMap['username'],
+      email: jsonMap['email'],
+      password: jsonMap['password'],
+      re_password: jsonMap['re_password'],
+      administrator: jsonMap['administrator']
+    );
+    return result;
+  }
+}
