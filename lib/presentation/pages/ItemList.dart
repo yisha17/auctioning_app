bd6860a2a9d152aa153198a2dccd1f4e261ead06
saron-solomon <it.saron.solomon@gmail.com>
2021-09-01 09:19:02 +0300

    list item edited

--- lib/presentation/pages/ItemList.dart
+++ lib/presentation/pages/ItemList.dart
@@ -4,3 +4,7 @@ import 'package:auction_real/business_logic/bloc/listitem_bloc.dart';
 import 'package:auction_real/data/dataproviders/itemDataProvider.dart';
 import 'package:auction_real/data/repositories/itemRepository.dart';
 import 'package:auction_real/presentation/pages/BiddingPage.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
