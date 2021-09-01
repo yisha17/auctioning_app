4dc039930b73c6fc6196f161dd4e7336b5b447e8
unknown <tiya.toti54@gmail.com>
2021-09-01 01:49:47 -0700

    calender cubit changed

--- lib/business_logic/cubits/cubit/calendar_cubit.dart
+++ lib/business_logic/cubits/cubit/calendar_cubit.dart
@@ -1,8 +1,52 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'calendar_state.dart';
 
 class CalendarCubit extends Cubit<CalendarState> {
-  CalendarCubit() : super(CalendarInitial());
+  CalendarCubit(this.context) : super(CalendarState(DateTime.now()));
+  BuildContext context;
+  Future pickFromDateTime({required bool pickDate}) async {
+    DateTime fromDate = DateTime.now();
+
+    final date = await pickDateTime(fromDate, pickDate: pickDate);
+
+    if (date == null) return;
+
+    date.isBefore(fromDate)
+        ? emit(CalendarState(fromDate))
+        : emit(CalendarState(date));
+  }
+
+  Future<DateTime?> pickDateTime(
+    DateTime initialDate, {
+    required bool pickDate,
+    DateTime? firstDate,
+  }) async {
+    if (pickDate) {
+      final date = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: firstDate ?? DateTime(2015, 8),
+        lastDate: DateTime(2101),
+      );
+
+      if (date == null) return null;
+      final time =
+          Duration(hours: initialDate.hour, minutes: initialDate.minute);
+
+      return date.add(time);
+    } else {
+      final timeOfDay = await showTimePicker(
+          context: context, initialTime: TimeOfDay.fromDateTime(initialDate));
+
+      if (timeOfDay == null) return null;
+
+      final date =
+          DateTime(initialDate.year, initialDate.month, initialDate.day);
+      final time = Duration(hours: timeOfDay.hour, minutes: timeOfDay.minute);
+      return date.add(time);
+    }
+  }
 }
