02df0e6742a916ba1ab8f13dfcc4dcc4b4a5a434
saron-solomon <it.saron.solomon@gmail.com>
2021-09-01 09:30:48 +0300

    user block edited

--- lib/business_logic/bloc/edituser_bloc.dart
+++ lib/business_logic/bloc/edituser_bloc.dart
@@ -5,3 +5,35 @@ import 'package:auction_real/data/models/models.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../../sharedInfo.dart';
+
+part 'edituser_event.dart';
+part 'edituser_state.dart';
+
+class EdituserBloc extends Bloc<EdituserEvent, EdituserState> {
+  EdituserBloc() : super(EdituserState());
+  final sharedInfo = SharedInfo();
+  void onEdit(String name,String email,String password,String re_password){
+      add(EditEvent(name:name,email: email,password:password,re_password: re_password));
+  }
+  @override
+  Stream<EdituserState> mapEventToState(
+    EdituserEvent event,
+  ) async* {
+
+    if (event is EditEvent){
+      try{
+        yield LoadingState();
+        await Future.delayed(Duration(seconds: 3));
+        var id = sharedInfo.getUserID();
+        UserModel user= UserModel(
+          name:event.name?.trim().toString(),
+          email: event.email?.trim().toString(),
+          password:event.password?.trim().toString(),
+          re_password: event.re_password?.trim().toString());
+        final data = await UserDataProvider().updateProfile(id!, user);
+        yield EdituserState(user: data);
+      }catch(e){
+        yield GetFailureState(e.toString());
+      }
+    }
