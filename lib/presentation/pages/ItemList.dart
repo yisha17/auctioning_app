31d90a73d9734d032fa644a6cea3f404880644ca
saron-solomon <it.saron.solomon@gmail.com>
2021-09-01 09:22:10 +0300

    list item edited

--- lib/presentation/pages/ItemList.dart
+++ lib/presentation/pages/ItemList.dart
@@ -4,10 +4,12 @@ import 'package:auction_real/business_logic/bloc/listitem_bloc.dart';
 import 'package:auction_real/data/dataproviders/itemDataProvider.dart';
 import 'package:auction_real/data/repositories/itemRepository.dart';
 import 'package:auction_real/presentation/pages/BiddingPage.dart';
+
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 class ItemList extends StatelessWidget {
   final String category;
   ItemList({required this.category});
@@ -58,3 +60,98 @@ class ItemList extends StatelessWidget {
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RepositoryProvider.value(
+        value: this.itemRepository,
+        child: BlocProvider(
+          create: (context) => ListitemBloc(item: this.itemRepository)
+            ..add(CourseLoad(category)),
+          child: BlocBuilder<ListitemBloc, ListitemState>(
+            builder: (_, state) {
+              if (state is CourseOperationFailure) {
+                return Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back),
+                        onPressed: () {
+                          Navigator.of(context, rootNavigator: true).pop();
+                        },
+                      ),
+                    ),
+                    Center(
+                      child: Text('could not get items'),
+                    ),
+                  ],
+                );
+              }
+              if (state is CourseOperationSuccess) {
+                final items = state.items;
+                return Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 150.0,
+                        decoration: new BoxDecoration(
+                          color: Color(0xFF42A5F5),
+                          boxShadow: [new BoxShadow(blurRadius: 40.0)],
+                          borderRadius: new BorderRadius.vertical(
+                            bottom: new Radius.elliptical(
+                                MediaQuery.of(context).size.width, 100.0),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: MediaQuery.of(context).size.width * 0.3,
+                        child: Text(
+                          category,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 20,
+                        child: IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            color: Colors.white,
+                            onPressed: () {
+                              Navigator.of(context, rootNavigator: true).pop();
+                            }),
+                      ),
+                      Positioned(
+                        top: 70,
+                        height: 600,
+                        width: MediaQuery.of(context).size.width,
+                        child: ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (_, index) {
+                            return ListTile(
+                              leading: Image.file(
+                                  File('${items.elementAt(index).image}')),
+                              title: Text('${items.elementAt(index).itemName}'),
+                              subtitle:
+                                  Text('${items.elementAt(index).category}'),
+                              trailing: Text(
+                                  '${items.elementAt(index).minPrice} Birr'),
+                              onTap: () {
+                                print('${items.elementAt(index).id}');
+                                 Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => BiddingPage(id:'${items.elementAt(index).id}',)));
+                              },
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
