e6899ce8ac7035cd63ee7b932f22cf13b4b5dd6c
saron-solomon <it.saron.solomon@gmail.com>
2021-09-01 09:34:30 +0300

    userevent edited

--- lib/business_logic/bloc/edituser_event.dart
+++ lib/business_logic/bloc/edituser_event.dart
@@ -6,3 +6,13 @@ abstract class EdituserEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+class EditEvent extends EdituserEvent{
+  final String ?name;
+  final String ?email;
+  final String ?password;
+  final String ?re_password;
+
+  EditEvent({this.name,this.email, this.password,this.re_password});
+}
+
+class CheckEditEvent extends EdituserEvent{}
