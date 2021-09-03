adfeff2c106f938ecbc391e9b938493b28d2c327
dathrob <dev.misganaw.meseret@gmail.com>
2021-09-03 12:47:53 +0300

    item event started

--- lib/business_logic/bloc/item_event.dart
+++ lib/business_logic/bloc/item_event.dart
@@ -1,4 +1,33 @@
 part of 'item_bloc.dart';
 
-@immutable
-abstract class ItemEvent {}
+class ItemCreateEvent extends Equatable {
+  const ItemCreateEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class ItemEvent extends ItemCreateEvent {
+  final String? itemName;
+  final String? user;
+  final int? increment;
+  final int? minPrice;
+  final String? closingDate;
+  final String? closingTime;
+  final List? image;
+  ItemEvent({
+    this.itemName,
+    this.user,
+    this.increment,
+    this.minPrice,
+    this.closingDate,
+    this.closingTime,
+    this.image,
+  });
+}
+
+class CalanderEvent extends ItemCreateEvent {}
+
+class CameraEvent extends ItemCreateEvent {}
+
+class OnSubmitEvent extends ItemCreateEvent {}
