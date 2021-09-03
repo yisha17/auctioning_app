741fb08126b97935bf33e2f160de8a63ab61e7f9
dathrob <dev.misganaw.meseret@gmail.com>
2021-09-03 12:47:24 +0300

    item block started

--- lib/business_logic/bloc/item_bloc.dart
+++ lib/business_logic/bloc/item_bloc.dart
@@ -1,18 +1,40 @@
-import 'dart:async';
-
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'item_event.dart';
+import 'package:auction_real/data/models/item_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'item_state.dart';
-
-class ItemBloc extends Bloc<ItemEvent, ItemState> {
-  ItemBloc() : super(ItemInitial());
+part 'item_event.dart';
+class ItemCreateBloc extends Bloc<ItemCreateEvent,ItemCreateState>{
+  ItemCreateBloc(this.context):super(ItemCreateState());
+  BuildContext context;
+  void onCreate(
+    String itemName,
+    String user,
+    int increment,
+    int minPrice,
+    String closingDate,
+    String closingTime,
+    final List image){
+      add(ItemEvent(itemName:itemName,
+      user: user,
+      increment: increment,
+      minPrice: minPrice,
+      closingDate: closingDate,
+      closingTime: closingTime,
+      image: image,));
+    }  
 
   @override
-  Stream<ItemState> mapEventToState(
-    ItemEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  Stream<ItemCreateState> mapEventToState(ItemCreateEvent event) async* {
+     if(event is ItemEvent){
+       try{
+         yield LoadingState();
+         await Future.delayed(Duration(seconds: 2));
+         ItemModel item= ItemModel(
+           itemName: event.itemName?.trim().toString(),
+           user: "",
+           increment:event.increment?.trim().toString());
+       }
+     }
   }
 }
