6e6a7f7de1c0f62d479ab31ab071e5611851f7b4
dathrob <dev.misganaw.meseret@gmail.com>
2021-09-05 14:56:03 +0300

    classes for allitemstate

--- lib/business_logic/bloc/allitem_state.dart
+++ lib/business_logic/bloc/allitem_state.dart
@@ -6,3 +6,16 @@ abstract class AllitemState extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class ItemSuccess extends AllitemState {
+  final List<ItemModel> items;
+
+  ItemSuccess([this.items = const []]);
+
+  @override
+  List<Object> get props => [items];
+}
+
+class LoadingState extends AllitemState {}
+
+class UserFailure extends AllitemState {}
