7524bb48537fd13fcae818a048b4542b6dcc85f0
Yisak <it.yisak.wondim@gmail.com>
2021-09-11 10:52:03 +0200

    listitem updated

--- lib/business_logic/bloc/listitem_event.dart
+++ lib/business_logic/bloc/listitem_event.dart
@@ -1,4 +1,17 @@
 part of 'listitem_bloc.dart';
 
-@immutable
-abstract class ItemlistEvent {}
+abstract class ListitemEvent extends Equatable {
+  const ListitemEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+
+class CourseLoad extends ListitemEvent {
+  final String category;
+  const CourseLoad(this.category);
+
+  @override
+  List<Object> get props => [];
+}
--- lib/business_logic/bloc/listitem_state.dart
+++ lib/business_logic/bloc/listitem_state.dart
@@ -1,6 +1,21 @@
 part of 'listitem_bloc.dart';
 
-@immutable
-abstract class ItemlistState {}
+abstract class ListitemState extends Equatable {
+  const ListitemState();
+  
+  @override
+  List<Object> get props => [];
+}
 
-class ItemlistInitial extends ItemlistState {}
+class CourseLoading extends ListitemState {}
+
+class CourseOperationSuccess extends ListitemState {
+  final Iterable<ItemModel> items;
+
+  CourseOperationSuccess([this.items = const []]);
+
+  @override
+  List<Object> get props => [items];
+}
+
+class CourseOperationFailure extends ListitemState {}
